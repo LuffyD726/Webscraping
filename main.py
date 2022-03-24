a445f95ba87e0a0cac559233ec9d359bc64c6e16
ori <orimalka112@gmail.com>
2022-03-24 21:38:12 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,53 @@
+from bs4 import BeautifulSoup
+import requests
+import webbrowser
+
+
+
+print('name, collateral object')
+URL = 'https://en.wikipedia.org/wiki/List_of_animal_names'
+response = requests.get(URL)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# find all the tables on the page
+All_tables = soup.findChildren('table')
+
+# find the specific table on the page
+my_table = All_tables[2]
+
+# find the body of the table
+my_tbody = my_table.find('tbody')
+
+# finds all the rows in the body
+rows = my_tbody.find_all('tr')
+
+
+
+# loop through all the rows on the body
+for row in rows:
+
+    # find every cell
+    value = row.find_all('td')
+
+    # strip the data from the cell
+    clear_value = [dp.text.strip() for dp in value]
+    # check if there is an empty cell (like the first one)
+    if len(clear_value) == 0:
+        continue
+    if clear_value[5] == '?':
+        print(clear_value[0])
+    else:
+        print(clear_value[0] + ', ' + clear_value[5] )
+
+
+f = open('helloworld.html', 'w')
+
+message = """<html>
+<head></head>
+<body><p>Hello World!</p></body>
+</html>"""
+
+f.write(message)
+f.close()
+
+webbrowser.open_new_tab('helloworld.html')
