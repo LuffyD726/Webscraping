8818273dd28ee29df49e691f36205a8076c599ba
ori <orimalka112@gmail.com>
2022-04-01 18:00:59 +0300

    html bonus

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
 import webbrowser
-import shutil
-import urllib.request
 
-print('name, collateral object')
+
+#open the html file
+f=open("Output.html", "w", encoding='utf-8')
+
+
+start ='''
+<html>
+<body><h2> Name, Collateral adjective <h2>'''
+
+
+#write the start of the html file
+f.write(start)
+
+print('Name, Collateral adjective')
 
 #Request the wikipedia page
 URL = 'https://en.wikipedia.org/wiki/List_of_animal_names'
@@ -34,19 +45,32 @@ for row in rows:
     clear_value = [dp.get_text(strip=True, separator=' ')  for dp in value]
 
     # check if there is an empty cell (like the first one)
-    if len(clear_value) == 0:
+    if len(clear_value) == 0 :
         continue
     elif '/' in clear_value[0]:
         clear_value[0] = clear_value[0].replace("/"," or ")
     elif '(list)' in clear_value[0]:
         clear_value[0] = clear_value[0].replace("(list)","")
         
-    #remove the empty colleteral adjactives cells
+    #remove the empty colleteral adjactive cells
     if clear_value[5] == '?':
-        print(clear_value[0])
+        print(clear_value[0])   
     else:
-        #image_url = 'https://en.wikipedia.org/wiki/'
-        #full_path = '.tmp/' + clear_value[0] + '.jpg'
-        #urllib.request.urlretrieve(image_url + clear_value[0], full_path)
-        print(clear_value[0] + ', ' + clear_value[5] )
+        Output = clear_value[0] + ', ' + clear_value[5] 
+        add_names = '''
+        <h3>'''+str(Output) + ''' <h3>'''
+
+        #add every name and collateral adjactive to the html file
+        f.write(add_names)
+        print(Output)
+
+end = '''<body>
+</html>
+'''
+
+f.write(end)
+f.close
+
+webbrowser.open_new_tab("Output.html")
+
 
